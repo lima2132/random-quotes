4900b46889e2c3faba655462806082571f4e0c7b
Lima <kir.lifshic11@gmail.com>
2025-02-07 15:41:19 +0600

    create random quotes

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const quotes = [
+	'The only way to do great work is to love what you do, - Steve Jobs',
+	'Innovation distinguishes a leader and a follower, - Steve Jobs',
+	"Your time is limited, so don't waste it living someone else's life. -Steve jobs",
+];
+
+const quoteElement = document.getElementById('quote');
+const generateBtn = document.getElementById('generate-btn');
+
+function generateRandomQuote() {
+	const randomIndex = Math.floor(Math.random() * quotes.length);
+
+	const randomQuote = quotes[randomIndex];
+	quoteElement.textContent = randomQuote;
+}
+
+generateBtn.addEventListener('click', generateRandomQuote);
